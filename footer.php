cf06bddf76cf07467a0e85ef546b1153c5c82e28
Kyler <kyler@kylerberry.com>
2012-11-09 04:01:57 -0500

    fixed footer, header, home added social images

--- footer.php
+++ footer.php
@@ -1,24 +1,35 @@
-<footer class="row">
-    <div class="twelve columns">
-      <hr />
-      <div class="row">
-        <div class="six columns">
-          <p>This site is not official and is an assignment for a UCF Digital Media course
-            <br />Designed By: Group 5, Twixel Inc.</p>
-        </div>
-        <div class="six columns">
-          <ul class="link-list right">
-            <li><a href="home.php">Home</a></li>
-            <li><a href="catalog.php">Products</a></li>
-            <li><a href="client.php">Account</a></li>
-            <li><a href="cart.php">Cart (0)</a></li>
-            <li><a href="contact.php">Contact Us</a></li>
-          </ul>
-        </div>
-      </div>
-    </div> 
-  </footer>
-    
+<div class="footer-container">
+  <footer class="row">
+          <div class="three columns">
+            <h4>Social</h4>
+            <ul class="left">
+              <li><a href="http://www.facebook.com/kyberry" title=""><img src="img/social/facebook.png" width="30" alt="facebook"/></a></li>
+              <li><a href="http://www.twitter.com/KylerBerry" title=""><img src="img/social/twitter.png" width="30" alt="twitter"/></a></li>
+              <li><a href="http://www.linkedin.com/pub/kyler-berry/43/486/125" title=""><img src="img/social/gplus.png" width="34" alt="linkedin"/></a></li>
+              <li><a href="#" title=""><img src="img/social/rss-large.png" width="30" alt="rss"/></a></li>
+            </ul>
+          </div>
+
+            <div class="three columns">
+            <h4>Contact Us</h4>
+            <p>We love our customers and we'd love to hear from you.</p>
+            <a href="contact.php" class="radius button left">Contact Us</a>
+          </div>
+
+          <div class="six columns">
+            <div>
+              <h4>About</h4>
+              <p>Twixel is the brain-child of five digital media students at University of Central Florida. Here we offer a daily deals site for web developers, designers, and creative professionals alike. Work Between the Pixel. Twixel.</p>
+            </div>
+            <div>
+              <h4>Disclaimer</h4>
+              <p>This site is not official website, it is an assignment for a UCF Digital Media course.</p>
+            </div>
+          </div>
+
+    </footer>
+</div>
+
     <!-- Included JS Files (Compressed) -->
     <script src="js/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
